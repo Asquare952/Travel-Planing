84936ecceea3f9d1b349a76b6328aae5718717b1
unknown <Asquare952>
2023-12-24 11:33:54 +0100

    Added more features

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Styles from "./home.module.css";
 import NavBar from "../../Component/header/NavBar";
 import Hero from "../../Component/Hero/Hero"
+import Review from "../../Component/Review/Review";
 const Home = () => {
   return (
     <>
     <NavBar/>
       <div className={Styles.homePage}>
         <Hero />
+        <Review/>
       </div>
     </>
   );
