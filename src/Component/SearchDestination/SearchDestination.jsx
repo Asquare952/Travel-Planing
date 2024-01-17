1d2a87497142c09823eaa621ba1fad71e87cc188
unknown <Asquare952>
2024-01-17 18:32:17 +0100

    Added more features on subpage

--- src/Component/SearchDestination/SearchDestination.jsx
+++ src/Component/SearchDestination/SearchDestination.jsx
@@ -1,4 +1,7 @@
 import Styles from "./destination.module.css";
+import { CiSearch } from "react-icons/ci";
+import { FaCalendarAlt } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
 const Deals = () => {
   return (
     <>
@@ -6,38 +9,43 @@ const Deals = () => {
         <div className={Styles.title}>
           <h4>Find deals on hotels, homes and much more...</h4>
         </div>
-
         <form className={Styles.destinationForm}>
-          <div>
-            <select id={Styles.destination}>
-              <option value="Where are you going?">Where are you going?</option>
-              <option value="">America</option>
-              <option value="">China</option>
-              <option value="">United Kindom</option>
-              <option value="">France</option>
-              <option value="">Saudi Arabia</option>
-              <option value="">Qatar</option>
-            </select>
+          <div className={Styles.formTitle}>
+            <h4>Search</h4>
           </div>
-          <div>
-            <select name="Check-in Check-out" id={Styles.check}>
-              <option value="Check-in - Check-out">Check-in - Check-out</option>
-              <option value=""></option>
-              <option value=""></option>
-              <option value=""></option>
-              <option value=""></option>
-            </select>
+          <div className={Styles.inputs}>
+            <label For="Destination/property name">
+              Destination/property name
+            </label>
+            <CiSearch className={Styles.icons} />
+            <input type="text" placeholder="Gold Coast" />
           </div>
-          <div>
-            <select name="0-Chidren 0-Adult" id={Styles.adult}>
-              <option value="2 adults - 0 children">
-                2 adults - 0 children
-              </option>
-              <option value=""></option>
-              <option value=""></option>
-              <option value=""></option>
-              <option value=""></option>
-            </select>
+          <div className={Styles.inputs}>
+            <label For="Check-in date">Check-in date</label>
+            <FaCalendarAlt className={Styles.icons} />
+            <input
+              name="Check-in Check-out"
+              id={Styles.check}
+              placeholder="Check-in date"
+            />
+          </div>
+          <div className={Styles.inputs}>
+            <label For="Check-out date">Check-out date</label>
+            <FaCalendarAlt className={Styles.icons} />
+            <input
+              name="Check-out date"
+              id={Styles.adult}
+              placeholder="Check-out date"
+            />
+          </div>
+          <div className={Styles.inputs}>
+            <label For="Travellers">Travellers</label>
+            <CgProfile className={Styles.icons} />
+            <input
+              name="0-Chidren 0-Adult"
+              id={Styles.adult}
+              placeholder="2 adults - 0 childern"
+            />
           </div>
           <button className={Styles.btnSearch}>Search</button>
         </form>
